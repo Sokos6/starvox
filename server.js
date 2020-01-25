059d80d3af55b7015b07311812990378e240367e
Sokos6 <will.sokolowski@gmail.com>
2020-01-24 23:38:48 -0600

    working mongodb

--- server.js
+++ server.js
@@ -16,10 +16,28 @@ MongoClient.connect('mongodb+srv://sokos6:SUGR9hFP48xAMwHG@cluster0-9xdtb.mongod
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.set('view engine', 'ejs')
+//  res.render(view, locals)
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
+
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {quotes: result})
+  })
+})
+
+app.get('/', (req, res) => {
+  var cursor = db.collection('quotes').find()
+  db.collection('quotes').find().toArray(function(err, results) {
+    console.log(results)
+  })
+})
+
 
 app.post('/quotes', (req, res) => {
   db.collection('quotes').save(req.body, (err, result) => {
